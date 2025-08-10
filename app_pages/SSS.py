fb70d34184e839323a9af8eb9614bc0ccd863a28
Alparslan <alparslantuncel2013@gmail.com>
2025-08-10 14:08:33 +0300

    New data

--- app_pages/SSS.py
+++ app_pages/SSS.py
@@ -35,7 +35,7 @@ with st.expander("📝 Model hangi verilerle eğitildi?"):
 
 with st.expander("🔒 Verilerim kaydediliyor mu?"):
     st.write("""
-    Hayır. Girdiğiniz yorumlar "Herkese Açık Yayınla"e tıklanmadığı sürece **hiçbir şekilde kaydedilmez** ve tamamen gizli tutulur.
+    Hayır. Girdiğiniz yorumlar "Herkese Açık Yayınlaıklanmadığı sürece **hiçbir şekilde kaydedilmez** ve tamamen gizli tutulur.
     Uygulama sadece anlık olarak tahmin işlemi yapar.
     """)
 
